94047bc266aec29306d07fe8bb76811d86811115
Devika Krishnamurthy <devika.k02@ad.infosys.com>
2020-10-09 07:18:13 +0530

    Changes committing

--- src/com/sample/project/MainJava.java
+++ src/com/sample/project/MainJava.java
@@ -2,6 +2,6 @@ package com.sample.project;
 
 public class MainJava {
 public static void main(String[] args) {
-	System.out.println("Sample Project");
+	System.out.println("Sample Project for Git");
 }
 }
